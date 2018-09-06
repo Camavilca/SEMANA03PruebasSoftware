50654ffee609aab16157c21d1d6eade51bc97ad9
CAMAVILCA <orlando.camavilca@tecsup.edu.pe>
2018-09-06 11:52:46 -0500

    COMMIT DE ORLANDO CAMAVILCA CHAVEZ

---
+++ SEMANA02COMPARTIDO/src/main/java/com/tecsup/lab02/SEMANA02COMPARTIDO/App.java
@@ -0,0 +1,13 @@
+package com.tecsup.lab02.SEMANA02COMPARTIDO;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
